6bbc062d0a47a5516160b06223c81efc2cc4d080
Eugene <eugene.vintsiv@gmail.com>
2018-12-27 17:35:34 +0200

    Initial Commit

---
+++ HT-7-PB-client/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HT-7-PB-client
+//
+//  Created by Eugene on 12/27/18.
+//  Copyright © 2018 Eugene. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HT-7-PB-client/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HT-7-PB-client
+//
+//  Created by Eugene on 12/27/18.
+//  Copyright © 2018 Eugene. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

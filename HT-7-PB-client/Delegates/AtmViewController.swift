3775b03b3cca5639462c5bdc467c39cf6e1cd068
Eugene <eugene.vintsiv@gmail.com>
2019-01-07 03:15:26 +0200

    1) add simple Atm View cell for Atm Table View 2) add empty Delegate class fr AtmViewController

---
+++ HT-7-PB-client/Delegates/AtmViewController.swift
@@ -0,0 +1,6 @@
+//
+// Created by Eugene on 1/7/19.
+// Copyright (c) 2019 Eugene. All rights reserved.
+//
+
+import Foundation
---
+++ HT-7-PB-client/Views/ATM/AtmAddressViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AtmAddressViewCell.swift
+//  HT-7-PB-client
+//
+//  Created by Eugene on 1/7/19.
+//  Copyright © 2019 Eugene. All rights reserved.
+//
+
+import UIKit
+
+class AtmAddressViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
